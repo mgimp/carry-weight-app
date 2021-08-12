a83d51b183ab1389220855d61b7012672da2434d
mgimp <70447460+mgimp@users.noreply.github.com>
2021-08-12 15:56:18 -0700

    Initial Setup
    Initial setup of files to construct the app. Includes:
    - index.html, script.js, and style.css files
    - Located in script.js is a basic setup for a character profile object.
    
    Create README.md
    
    Update README.md
    
    Update README.md

---
+++ script.js
@@ -0,0 +1,20 @@
+let character = {
+    race: 'teifling',
+    STR: 13,
+    class: ['barbarian',3], // ['class name', level]
+    'carry weight up': 0, // 0 for no, 1 for yes
+    items: {
+        weapons: ['Short Sword','Dagger','Dagger'],
+        armor: 'Padded',
+        items: ['potion'],
+        rations: 10,
+        rope: 50 // length in feet
+    },
+    money: {
+        copper: 0,
+        silver: 0,
+        electrum: 0,
+        gold: 15,
+        platinum: 0
+     }
+}
